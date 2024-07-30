7a8704ae6df24993924a32fa9f9f5cdb11577b81
hoangtv090103 <hoangtv090103@gmail.com>
2024-07-30 18:38:23 +0700

    add login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,17 @@
-import { useEffect, useState } from 'react'
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 
-import './App.css'
-import axios from 'axios'
 function App() {
-  const [sampleText, setSampleTesxt] = useState('Hello World')
-  
-  useEffect(() => {
-    const getSampleText = async () => {
-      const res = await axios.get('/api/v1/texts',
-        {
-          headers: {
-            Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2YTIxNzU3MmM2ZjRkN2ZlNmIwMTU1NiIsImlhdCI6MTcyMjMyNDQ5NSwiZXhwIjoxNzIyNDEwODk1fQ.N9SPOrLZY6T2kavXwhJ3-13sBJvVQtle2Lo0OMKZvrI"
-          }})
-      const text = res.data.text;
-      setSampleTesxt(text)
-    }
-
-    getSampleText()
-  }, []);
-
   return (
     <>
-    <div>
-    <h1> Typing Practice </h1>
-        <p>{ sampleText }</p>
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
